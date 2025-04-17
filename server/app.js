5a303827f199abbe99b41414295c7f45b58a9b57
unknown <196222578+akk0020@users.noreply.github.com>
2025-04-16 21:17:37 -0500

    again

--- server/app.js
+++ server/app.js
@@ -43,7 +43,7 @@ const cors = (request, response, next) => {
 app.use(cors);
 app.use(express.json());
 app.use(logging);
-app.use("/restaurants", restaurants);
+// app.use("/restaurants", restaurants);
 
 app.get("/status", (request, response) => {
   response.send(JSON.stringify({ message: "Service healthy" }));
